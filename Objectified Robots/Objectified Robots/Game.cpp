10bf414ad6200d8260a3aa443a6013132cab1fab
guilhermewastaken <75986527+guilhermewastaken@users.noreply.github.com>
2021-05-23 09:24:00 +0100

    Game Class Update (2)
    Added all the collisions private methods

--- Objectified Robots/Objectified Robots/Game.cpp
+++ Objectified Robots/Objectified Robots/Game.cpp
@@ -39,6 +39,50 @@ Game::Game(const string& filename) : maze(1, 1), player(1, 1) {
 			numberOfRow++;
 		}
 	}
+}
 
+//Collisions
+bool Game::collision(Robot& robot, Post& post) {
+	if ((robot.getPosX() == post.getPosX()) && (robot.getPosY() == post.getPosY())) { //Both at the same position
+		robot.kill(); //The robot is killed immediatly, but where it will lie dead will be decided in the caller method
+		return true;
+	}
+	return false; //No collision
+}
 
+bool Game::collision(Robot& robot, Player& player) {
+	if ((robot.getPosX() == player.getPosX()) && (robot.getPosY() == player.getPosY())) { //Both at the same position
+		if (robot.isAlive()) {
+			player.kill();
+			return true; //The player hit a functioning robot, he dies
+		}
+		return true; //The player hit a dead robot, he doesn't die
+	}
+	return false; //No collision
+}
+
+bool Game::collision(Robot& robot1, Robot& robot2) {
+	if ((robot1.getPosX() == robot2.getPosX()) && (robot1.getPosY() == robot2.getPosY())) { //Both at the same position
+		robot1.kill();
+		robot2.kill();
+		return true; //The robots hit eachother, both die (doesn't matter if they're allready dead
+		}
+	return false; //No collision
+}
+
+bool Game::collision(Post& post, Player& player) {
+	if ((post.getPosX() == player.getPosX()) && (post.getPosY() == player.getPosY())) { //Both at the same position
+		if (post.getType() == "Electrified") {
+			player.kill(); //The player hit an electrified robot, he dies
+			return true;
+		}
+		return true; //The player hit either an exit or an electrified post. Game::victory() will be used next
+	}
+}
+
+bool Game::victory(Post& post, Player& player) const { //This method assumes the player and the post are in the same position
+	if (post.getType() == "Exit") {
+		return true; //The player hit an exit
+	}
+	return false;
 }
--- Objectified Robots/Objectified Robots/Game.h
+++ Objectified Robots/Objectified Robots/Game.h
@@ -6,6 +6,7 @@
 #include <vector>
 #include <fstream>
 #include <sstream>
+
 #include "Robot.h"
 #include "Post.h"
 #include "Maze.h"
@@ -16,27 +17,34 @@
 using namespace std;
 
 class Game {
+private: //Atributes
+	Maze maze;
+	Player player;
+	vector<Robot> robotList;
+	vector<Exit> exitList;
+
 public:
 	Game(const string& filename);
 	// This constructor should initialize the Maze, the vector of Robots, and the Player,
 	// using the chars read from the file
 	bool play(); // implements the game loop; returns true if player wins, false otherwise
 	bool isValid();
-private:
-	void showGameDisplay() const;
-	bool collide(Robot& robot, Post& post); // check if robot collided with post (and possibly set it as dead)
-	bool collide(Robot& robot, Player& player); // check if human and robot collided (and possibly set human as dead)
-	// other methods, for example:
+
+private: //Private methods
+	void print() const;
+
+	//Collisions
+	bool collision(Robot& robot, Post& post); // Checks if robot collided with post (and possibly kill robot)
+	bool collision(Robot& robot, Player& player); // Checks if player and robot collided (and possibly kill player)
+	bool collision(Robot& robot1, Robot& robot2); //Checks if two robots collided (and possibly kill both)
+	bool collision(Post& post, Player& player); //Checks if player collided with post (and possibly kill player)
+	bool victory(Post& post, Player& player) const; //Checks if the player hit an exit
+	//Must only be used after a collision with a post has been established
+
+												  // other methods, for example:
 	// to check if player is trying to move to a valid place
 	// to apply a valid play and check collisions
-	// to check if two robots collide (and possibly set them as dead)
-	// etc.
-private:
-	Maze maze;
-	Player player;
-	vector<Robot> robotList;
-	vector<Exit> exitList;
-};
+}
 
 
 #endif
